2799766579554019fc3e1805bdb11eeabfe0e8d0
Joell Malone <joellmalone000@gmail.com>
2023-11-13 20:15:15 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// multiply
+//✓ is an equation whose multiplied results will equal 62
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+// random
+//✓ 1) generates a random integer greater than 0
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+// mod
+//✓ is an equation that calculates a remainder that is equal to 4
+
+let num3 = 22;
+let num4 = 6;
+const mod = Math.round(num3 / num4);
+
+// max
+//✓ will return 20 as the highest number in the set:
+
+let max = Math.max(10, 15, 20);
